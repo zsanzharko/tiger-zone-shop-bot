1653f69a9766fa2b6827190d6ec6b504ae2ab7a9
Sanzhar Zhanibekov <semid46@gmail.com>
2023-05-29 00:11:54 +0600

    fixed rows column in inline keyboards

--- src/main/java/kz/tiger/zone/tigerzoneshopbot/bot/config/TelegramMessageConfig.java
+++ src/main/java/kz/tiger/zone/tigerzoneshopbot/bot/config/TelegramMessageConfig.java
@@ -2,4 +2,5 @@ package kz.tiger.zone.tigerzoneshopbot.bot.config;
 
 public class TelegramMessageConfig {
     public static final String CALLBACK_DELIMITER = ":";
+    public static final int MESSAGE_COLUMNS = 2;
 }
--- src/main/java/kz/tiger/zone/tigerzoneshopbot/bot/utils/KeyboardFactory.java
+++ src/main/java/kz/tiger/zone/tigerzoneshopbot/bot/utils/KeyboardFactory.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import static kz.tiger.zone.tigerzoneshopbot.bot.config.TelegramMessageConfig.CALLBACK_DELIMITER;
+import static kz.tiger.zone.tigerzoneshopbot.bot.config.TelegramMessageConfig.MESSAGE_COLUMNS;
 
 public class KeyboardFactory {
 
@@ -19,8 +20,14 @@ public class KeyboardFactory {
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
         List<InlineKeyboardButton> rowInline = new ArrayList<>();
         InlineKeyboardButton button;
-        for (CategoryModel model : categoryModelList) {
+
+        for (int i = 0; i < categoryModelList.size(); i++) {
+            CategoryModel model = categoryModelList.get(i);
             button = new InlineKeyboardButton();
+            if (i % MESSAGE_COLUMNS == 0) {
+                rowsInline.add(rowInline);
+                rowInline = new ArrayList<>();
+            }
             button.setText(model.getTitle());
             button.setCallbackData("category" + CALLBACK_DELIMITER + model.getId());
             rowInline.add(button);
@@ -35,11 +42,17 @@ public class KeyboardFactory {
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
         List<InlineKeyboardButton> rowInline = new ArrayList<>();
         InlineKeyboardButton button;
+        int i = 0;
         for (Map.Entry<String, CallbackCommandType> typeEntry : menu.entrySet()) {
             button = new InlineKeyboardButton();
+            if (i % MESSAGE_COLUMNS == 0) {
+                rowsInline.add(rowInline);
+                rowInline = new ArrayList<>();
+            }
             button.setText(typeEntry.getKey());
             button.setCallbackData(typeEntry.getValue().toString());
             rowInline.add(button);
+            i++;
         }
         rowsInline.add(rowInline);
         inlineKeyboard.setKeyboard(rowsInline);
